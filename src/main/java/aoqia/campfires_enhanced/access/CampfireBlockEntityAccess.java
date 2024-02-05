d6ac73f1baeebfd23f996e9afcdc32d734b98685
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-02-06 04:23:00 +1100

    refactor(class) - Refactored name of CampfireBlockEntityAccess.
    Refactored name of CampfireBlockEntityAccess to CampfireBlockEntityTimer.

--- src/main/java/aoqia/campfires_enhanced/access/CampfireBlockEntityAccess.java
+++
@@ -1,9 +0,0 @@
-package aoqia.campfires_enhanced.access;
-
-public interface CampfireBlockEntityAccess {
-    long getElapsedTicks();
-    void setElapsedTicks(long ticks);
-    void increaseElapsedTicks();
-    void decreaseElapsedTicks();
-    void resetElapsedTicks();
-}
---
+++ src/main/java/aoqia/campfires_enhanced/access/CampfireBlockEntityTimer.java
@@ -0,0 +1,9 @@
+package aoqia.campfires_enhanced.access;
+
+public interface CampfireBlockEntityTimer {
+    long campfires_enhanced$getElapsedTicks();
+    void campfires_enhanced$setElapsedTicks(long ticks);
+    void campfires_enhanced$increaseElapsedTicks();
+    void campfires_enhanced$decreaseElapsedTicks();
+    void campfires_enhanced$resetElapsedTicks();
+}
