60e83611af2ec37720e61443ce43e381e6ca3d6b
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-02-06 04:25:43 +1100

    feat(provider) - Added EnglishLanguageProvider and RecipeProvider.

--- src/main/java/aoqia/campfires_enhanced/CampfiresEnhancedDataGenerator.java
+++ src/main/java/aoqia/campfires_enhanced/CampfiresEnhancedDataGenerator.java
@@ -1,11 +1,15 @@
 package aoqia.campfires_enhanced;
 
+import aoqia.campfires_enhanced.providers.CEEnglishLanguageProvider;
+import aoqia.campfires_enhanced.providers.CERecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class CampfiresEnhancedDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
-	}
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(CEEnglishLanguageProvider::new);
+        pack.addProvider(CERecipeProvider::new);
+    }
 }
